e73a1a04e2ff2c77908181059070f5a80da215be
Melisha <melisha.evans06@gmail.com>
2023-08-31 18:53:04 -0700

    updated header error

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
